9652edc49632f4bdc86fa8365ff25fd7c8b37df2
M4asl <mateusz.maslowiec98@gmail.com>
2022-11-12 20:39:15 +0100

    Create sample data

---
+++ src/utils/sampleData.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    title: 'Pineapple',
+    mediaUrl:
+      'https://images.unsplash.com/photo-1589820296156-2454bb8a6ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+  {
+    title: 'Banana',
+    mediaUrl:
+      'https://images.unsplash.com/photo-1528825871115-3581a5387919?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80',
+  },
+  {
+    title: 'Green Peas',
+    mediaUrl:
+      'https://images.unsplash.com/photo-1592394533824-9440e5d68530?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+  },
+  {
+    title: 'Orange',
+    mediaUrl:
+      'https://images.unsplash.com/photo-1557800636-894a64c1696f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80',
+  },
+  {
+    title: 'Strawberry',
+    mediaUrl:
+      'https://images.unsplash.com/photo-1623227866882-c005c26dfe41?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80',
+  },
+  {
+    title: 'Avocado',
+    mediaUrl:
+      'https://images.unsplash.com/photo-1523049673857-eb18f1d7b578?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1375&q=80',
+  },
+  {
+    title: 'Apple',
+    mediaUrl:
+      'https://images.unsplash.com/photo-1590005354167-6da97870c757?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1162&q=80',
+  },
+  {
+    title: 'Lemon',
+    mediaUrl:
+      'https://images.unsplash.com/photo-1587324438673-56c78a866b15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80',
+  },
+  {
+    title: 'Mango',
+    mediaUrl:
+      'https://images.unsplash.com/photo-1591073113125-e46713c829ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+  },
+  {
+    title: 'Cherry',
+    mediaUrl:
+      'https://plus.unsplash.com/premium_photo-1661721423612-c3e678790a50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+];
