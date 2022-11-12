a18447f68697b8937bc5573f34a5b402e1455ad4
M4asl <mateusz.maslowiec98@gmail.com>
2022-11-12 21:20:00 +0100

    Create custom hook update mouse position & get coords of mouse

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Media from './components/Media';
 import Title from './components/Title';
 import sampleData from './utils/sampleData';
 import './styles/home.scss';
 
+const useMousePosition = () => {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    const updateMousePosition = (event) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener('mousemove', updateMousePosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition);
+    };
+  }, []);
+
+  return mousePosition;
+};
+
 const App = () => {
   const [activeIndex, setActiveIndex] = useState(-1);
+
+  const { x, y } = useMousePosition();
+
+  console.log(x, y);
+
   return (
     <div className="page-wrapper">
       <div className="project-list">
         {sampleData.map(({ title }, index) => (
           <Title
+            key={index}
             title={title}
             setActiveIndex={setActiveIndex}
             index={index}
@@ -20,7 +47,9 @@ const App = () => {
       <div className="project-media">
         {sampleData.map(({ mediaUrl }, index) => {
           const isActive = index === activeIndex;
-          return <Media url={mediaUrl} active={isActive} />;
+          return (
+            <Media url={mediaUrl} active={isActive} key={index} />
+          );
         })}
       </div>
     </div>
--- src/components/Media.js
+++ src/components/Media.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Media = ({ url, active }) => {
   return (
     <img
-      className={active && 'is-active'}
+      className={active ? 'is-active' : null}
       src={url}
       alt="sample data"
     />
