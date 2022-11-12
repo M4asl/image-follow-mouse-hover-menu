8aeaa215927d72f643e4bd45285c489d2c509fe9
M4asl <mateusz.maslowiec98@gmail.com>
2022-11-12 21:34:11 +0100

    Get current image dimension width & height

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useCallback, useLayoutEffect, useState } from 'react';
+
+const getDimensionObject = (node) => {
+  const rect = node.getBoundingClientRect();
+  return {
+    width: rect.width,
+    height: rect.height,
+  };
+};
+
+const useSize = () => {
+  const [dimensions, setDimensions] = useState({});
+  const [node, setNode] = useState(null);
+
+  const ref = useCallback((node) => {
+    setNode(node);
+  }, []);
+
+  useLayoutEffect(() => {
+    if (node) {
+      const measure = () => setDimensions(getDimensionObject(node));
+      measure();
+    }
+  }, []);
+
+  return [ref, dimensions];
+};
 
 const Media = ({ url, active, x, y }) => {
+  const [ref, { width, height }] = useSize();
   return (
     <img
       className={active ? 'is-active' : null}
+      ref={ref}
       src={url}
       alt="sample data"
-      style={{ transform: `translate(${x}px, ${y}px)` }}
+      style={{
+        transform: `translate(${x}px, ${y}px)`,
+      }}
     />
   );
 };
